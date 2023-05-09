02dd223edcc88c21dc86858e6440e352b963b24f
Adam Vinicius <adamvinicius@gmail.com>
2023-05-09 20:59:18 -0300

    aula 15,16,17 prefixos, pages, wait, tags, trycatch

--- src/test/java/br/com/chronosacademy/core/Driver.java
+++ src/test/java/br/com/chronosacademy/core/Driver.java
@@ -2,13 +2,19 @@ package br.com.chronosacademy.core;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Driver {
-    private WebDriver driver;
+    private static WebDriver driver;
+    private static WebDriverWait wait;
 
     public Driver(String navegador){
         switch (navegador){
@@ -33,10 +39,21 @@ public class Driver {
                 driver = new ChromeDriver();
                 break;
         }
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().window().maximize();
     }
 
-    public WebDriver getDriver() {
+    public static WebDriver getDriver() {
         return driver;
     }
+
+    public static void visibilityOf(WebElement element){
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public static void invisibilityOf(WebElement element){
+        wait.until(ExpectedConditions.invisibilityOf(element));
+    }
+
+
 }
---
+++ src/test/java/br/com/chronosacademy/pages/LoginPage.java
@@ -0,0 +1,60 @@
+package br.com.chronosacademy.pages;
+
+import br.com.chronosacademy.core.Driver;
+import br.com.chronosacademy.maps.LoginMap;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    LoginMap loginMap;
+
+    public LoginPage() {
+        loginMap = new LoginMap();
+        PageFactory.initElements(Driver.getDriver(), loginMap);
+    }
+
+    public void clickBtnLogin(){
+        Driver.visibilityOf(loginMap.btnLogin);
+        loginMap.btnLogin.click();
+    }
+
+    public void clickBtnFechar(){
+        loginMap.btnFechar.click();
+    }
+
+    public void clickDivFechaModal(){
+        loginMap.divFechaModal.click();
+    }
+
+    public void setInpUserName(String username){
+        loginMap.inpUserName.sendKeys(username);
+    }
+
+    public void setInpPassword(String password){
+        loginMap.inpPassword.sendKeys(password);
+    }
+
+    public void clickInpRemember(){
+        loginMap.inpRemember.click();
+    }
+
+    public void clickLinkCreateAccount(){
+        loginMap.linkCreateAccount.click();
+    }
+
+    public void clickBtnSignIn(){
+        loginMap.btnSignIn.click();
+    }
+
+    public boolean isBtnSignIn(){
+        return loginMap.btnSignIn.isEnabled();
+    }
+
+    public void visibilityOfBtnFechar(){
+        Driver.visibilityOf(loginMap.btnFechar);
+    }
+
+    public void invisibilityOfBtnFechar(){
+        Driver.invisibilityOf(loginMap.btnFechar);
+    }
+}
