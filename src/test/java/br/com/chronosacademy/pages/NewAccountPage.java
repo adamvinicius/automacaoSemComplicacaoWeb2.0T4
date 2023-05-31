29f0a5b23bf71a543b01b742a3376540f2ccf362
Adam Vinicius <adamvinicius@gmail.com>
2023-05-30 21:09:42 -0300

    aula 25 e 26, crud, create, read e agrupador

--- src/test/java/br/com/chronosacademy/pages/NewAccountPage.java
+++ src/test/java/br/com/chronosacademy/pages/NewAccountPage.java
@@ -3,6 +3,7 @@ package br.com.chronosacademy.pages;
 import br.com.chronosacademy.core.Driver;
 import br.com.chronosacademy.maps.NewAccountMap;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class NewAccountPage {
     NewAccountMap newAccountMap;
@@ -15,5 +16,34 @@ public class NewAccountPage {
     public String getTextNewAccount(){
         Driver.visibilityOf(newAccountMap.textCreateAccount);
         return newAccountMap.textCreateAccount.getText();
+    }
+
+    public void setInpUserName(String userName){
+        newAccountMap.inpUserName.sendKeys(userName);
+    }
+    public void setInpEmail(String email){
+        newAccountMap.inpEmail.sendKeys(email);
+    }
+
+    public void setInpPassword(String password){
+        newAccountMap.inpPassword.sendKeys(password);
+    }
+
+    public void setInpConfirmPassword(String password){
+        newAccountMap.inpConfirmPassword.sendKeys(password);
+    }
+
+    public void selectCountry(String country){
+        Select select = new Select(newAccountMap.slcCountry);
+        Driver.aguardaOptions(select);
+        select.selectByVisibleText(country);
+    }
+
+    public void clickInpIagree(){
+        newAccountMap.inpIagree.click();
+    }
+
+    public void clickBtnRegister(){
+        newAccountMap.btnRegister.click();
     }
 }
